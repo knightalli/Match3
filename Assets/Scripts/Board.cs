ccff8d121f20bdc072b37c2dd2b9819286bffa79
knightalli <knightalli@mail.ru>
2023-04-07 17:26:14 +0500

    thirs

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+    private BackgroundTile[,] allTiles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allTiles = new BackgroundTile[width, height];
+        SetUp();
+    }
+    
+    private void SetUp()
+    {
+        for (int i = 0; i < width; i++) 
+        { 
+            for (int j = 0; j < height; j++)
+            {
+                Vector2 tempPositoin = new Vector2(i,j);
+                GameObject backgroundTile = Instantiate(tilePrefab, tempPositoin,Quaternion.identity) as GameObject;
+                backgroundTile.transform.parent = this.transform;
+                backgroundTile.name = "(" + i + ", " + j + ")";
+            }
+        }
+    }
+}
