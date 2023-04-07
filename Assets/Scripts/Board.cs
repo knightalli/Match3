01f02551cc20972eaf6fb303192e88c47e711306
knightalli <knightalli@mail.ru>
2023-04-08 00:06:13 +0500

    five

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,38 +4,42 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    public int width;
+    public int width; // Размеры доски.
     public int height;
-    public GameObject tilePrefab;
-    private BackgroundTile[,] allTiles;
-    public GameObject[] dots;
-    public GameObject[,] allDotes;
+    public GameObject tilePrefab;  //Объект - ячейка.
+    private BackgroundTile[,] allTiles; //Все координаты ячеек.
+    public GameObject[] dots; //Все варианты значков (в нашем случае по вариации цветов).
+    public GameObject[,] allDotes; //Все координаты значков.
 
     // Start is called before the first frame update
     void Start()
     {
-        allTiles = new BackgroundTile[width, height];
+        allTiles = new BackgroundTile[width, height];  //Инициализируем ячейки и значки.
         allDotes = new GameObject[width, height];
-        SetUp();
+        SetUp(); //Устанавливаем и наполняем доску.
     }
     
     private void SetUp()
     {
         for (int i = 0; i < width; i++) 
         { 
-            for (int j = 0; j < height; j++)
+            for (int j = 0; j < height; j++) //Делаем цикл в цикле, чтобы захватить все ячейки по координатам, как в матрице.
             {
                 Vector2 tempPositoin = new Vector2(i,j);
-                GameObject backgroundTile = Instantiate(tilePrefab, tempPositoin,Quaternion.identity) as GameObject;
-                backgroundTile.transform.parent = this.transform;
-                backgroundTile.name = "(" + i + ", " + j + ")";
+                GameObject backgroundTile = Instantiate(tilePrefab, tempPositoin,Quaternion.identity) as GameObject; //Instantiate - метод по созданию объекта.
+                                                                                                                     //tilePrefab - сам объект.
+                                                                                                                     //tempPosition - его координаты.
+                                                                                                                     //Quaternion - вращение предмета.
+                                                                                                                     //.identify - оставляем его в первоначальном виде.
+                backgroundTile.transform.parent = this.transform; //Каждую ячейку мы делаем дочерней для Board.
+                backgroundTile.name = "(" + i + ", " + j + ")"; //Называем ячейку.
 
-                int dotToUse = Random.Range(0, dots.Length);
-                GameObject dot = Instantiate(dots[dotToUse], tempPositoin, Quaternion.identity);
-                dot.transform.parent = this.transform;
-                dot.name = "(" + i + ", " + j + ")";
+                int dotToUse = Random.Range(0, dots.Length); //Возвращает любое из вариантов значков по цвету.
+                GameObject dot = Instantiate(dots[dotToUse], tempPositoin, Quaternion.identity); //Создаем значок.
+                dot.transform.parent = this.transform; //Каждый значок мы делаем дочерним для Board.
+                dot.name = "(" + i + ", " + j + ")"; //Называем значок.
 
-                allDotes[i,j] = dot;
+                allDotes[i,j] = dot; //Заполняем доску.
             }
         }
     }
--- Assets/Scripts/Dot.cs
+++ Assets/Scripts/Dot.cs
@@ -10,13 +10,16 @@ public class Dot : MonoBehaviour
     private GameObject otherDot;
     private Vector2 firstTouchPosition;
     private Vector2 finalTouchPosition;
+    private Vector2 tempPosition;
     public float swipeAngle = 0;
     // Start is called before the first frame update
     void Start()
     {
-        board = FindObjectOfType<Board>();
-        targetX = (int)transform.position.x;
-        targetY = (int)transform.position.y;
+        board = FindObjectOfType<Board>(); //Берем уже инициализированную доску и работаем с ней.
+        targetX = (int)transform.position.x; //Берем позицию по x этого значка.
+        targetY = (int)transform.position.y; //Берем позицию по y этого значка.
+        row = targetY; //Назначаем строку и столбец данного значка.
+        column = targetX;
     }
 
     // Update is called once per frame
@@ -24,12 +27,38 @@ public class Dot : MonoBehaviour
     {
         targetX = column;
         targetY = row;
+        if (Mathf.Abs(targetX - transform.position.x) > 0.1)
+        {
+            //Подвинуться в направлении цели
+            tempPosition = new Vector2 (targetX, transform.position.y);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, 0.4f); //Делаем плавное передвижение значка по заданному направлению.
+        }
+        else
+        {
+            //Установить точную позицию
+            tempPosition = new Vector2(targetX, transform.position.y);
+            transform.position = tempPosition;
+            board.allDotes[column, row] = this.gameObject;
+        }
+        if (Mathf.Abs(targetY - transform.position.y) > 0.1)
+        {
+            //Подвинуться в направлении цели
+            tempPosition = new Vector2(transform.position.x, targetY);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, 0.4f);
+        }
+        else
+        {
+            //Установить точную позицию
+            tempPosition = new Vector2(transform.position.x, targetY);
+            transform.position = tempPosition;
+            board.allDotes[column, row] = this.gameObject;
+        }
     }
 
     private void OnMouseDown()
     {
         firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //Debug.Log(firstTouchPosition);
+        //Debug.Log(firstTouchPosition); Была проверка, что передает координаты точки.
     }
 
     private void OnMouseUp()
@@ -41,17 +70,39 @@ public class Dot : MonoBehaviour
     void CalculateAngle()
     {
         swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI;
-        //Debug.Log(swipeAngle);
+        //Debug.Log(swipeAngle); Была проверка, что вычисляет угол.
+        MovePieces(); 
     }
 
     void MovePieces()
     {
-        if (swipeAngle > -45 && swipeAngle <= 45)
+        if (swipeAngle > -45 && swipeAngle <= 45 && column < board.width)
         {
             //Движение направо
             otherDot = board.allDotes[column + 1, row];
             otherDot.GetComponent<Dot>().column -= 1;
             column += 1;
+        }
+        else if (swipeAngle > 45 && swipeAngle <= 135 && row < board.height)
+        {
+            //Движение вверх
+            otherDot = board.allDotes[column, row + 1];
+            otherDot.GetComponent<Dot>().row -= 1;
+            row += 1;
+        }
+        else if ((swipeAngle > 135 || swipeAngle <= -135) && column > 0)
+        {
+            //Движение налево
+            otherDot = board.allDotes[column - 1, row];
+            otherDot.GetComponent<Dot>().column += 1;
+            column -= 1;
+        }
+        else if (swipeAngle < -45 && swipeAngle >= -135 && row > 0)
+        {
+            //Движение вниз
+            otherDot = board.allDotes[column, row - 1];
+            otherDot.GetComponent<Dot>().row += 1;
+            row -= 1;
         }
     }
 }
